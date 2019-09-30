c3d3c9cfcba7e2fbdeb4ed3915a4d522994c00ab
Pavan Jadhaw <pavanjadhaw96@gmail.com>
2019-09-30 10:17:39 +0530

    [update] add author to easter

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const greeting = `Two roads diverged in a wood, and I—
 I took the one less traveled by,
-And that has made all the difference.`;
+And that has made all the difference.
+
+The Road Not Taken
+- BY ROBERT FROST`;
 
 console.log(greeting);
